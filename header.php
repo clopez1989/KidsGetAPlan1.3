39adea865221cd5922fcd1a03b2b982e477f4f86
Carlos Lopez <carlos.lopez@knights.ucf.edu>
2013-11-25 11:19:46 -0500

    Revert 18e1ee1..30d1e37
    This rolls back to commit 18e1ee12dbee5f3dbdd1279ea9abe4b86f132bd4.

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="js/jquery.mobile-1.3.2.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
-<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <script type="text/javascript" 	src="js/jquery.smint.js"></script>
 
 <script type="text/javascript">	
@@ -28,36 +27,9 @@
 </script>
 
 <script type="text/javascript">	
-		$(document).ready( function(){
-			$("#accordion").accordion({ collapsible: true });
-		});
-</script>
-
-<script type="text/javascript">
-$(function(){
-  $("a[rel='tab']").click(function(e){
-    //e.preventDefault();
-    /*
-    if uncomment the above line, html5 nonsupported browers won't change the url but will display the ajax content;
-    if commented, html5 nonsupported browers will reload the page to the specified link.
-    */
- 
-    //get the link location that was clicked
-    pageurl = $(this).attr('href');
- 
-    //to get the ajax content and display in div with id 'content'
-    $.ajax({url:pageurl+'?rel=tab',success: function(data){
-      $('#content').html(data);
-    }});
- 
-    //to change the browser URL to the given link location
-    if(pageurl!=window.location){
-      window.history.pushState({path:pageurl},'',pageurl);
-    }
-    //stop refreshing to the page given in
-    return false;
-  });
-});
+	$('#accordion a').click(function(){
+		return false;
+	});
 </script>
 
 </head>
